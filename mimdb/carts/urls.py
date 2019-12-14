b8af4a566aabd004203431e404ecb15938b96a4b
root <root@localhost.localdomain>
2019-12-14 23:01:27 +0000

    Remove from cart

--- mimdb/carts/urls.py
+++ mimdb/carts/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.view,name='cart'),
+    path('<int:id>/',views.remove_from_cart,name="remove_from_cart"),
     path('<slug:slug>/',views.add_to_cart,name="add_to_cart")
 ]
--- mimdb/carts/views.py
+++ mimdb/carts/views.py
@@ -11,6 +11,14 @@ def view(request):
         the_id=None
     if the_id:
         cart = Cart.objects.get(id=the_id)
+        #every time we view the cart it will actually update for us
+        sum=0.00
+        for item in cart.cartitem_set.all():
+            line_total = float(item.product.price)*item.quantity
+            sum+=line_total
+        request.session['items_total']=cart.cartitem_set.count()
+        cart.total=sum
+        cart.save()
         context = {"cart":cart}
     else:
         empty_message="Your cart is empty,Please Keep Shopping"
@@ -18,6 +26,20 @@ def view(request):
 
     template = "carts/view.html"
     return render(request,template,context)
+
+def remove_from_cart(request,id):
+    try:
+        the_id = request.session['cart_id']
+        cart = Cart.objects.get(id=the_id)
+    except:
+        return HttpResponseRedirect(reverse("cart"))    
+ 
+    cartitem = CartItem.objects.get(id=id)
+    #cartitem.delete()
+    cartitem.cart = None
+    cartitem.save()
+    #send  "Success message"
+    return HttpResponseRedirect(reverse("cart"))
 
 def add_to_cart(request,slug):
     request.session.set_expiry(120000)
@@ -58,14 +80,7 @@ def add_to_cart(request,slug):
             cart_item.variations.add(*product_var)
         cart_item.quantity = qty
         cart_item.save()
-        sum=0.00
-
-        for item in cart.cartitem_set.all():
-            line_total = float(item.product.price)*item.quantity
-            sum+=line_total
-        
-        request.session['items_total']=cart.cartitem_set.count()
-        cart.total=sum
-        cart.save()
+        #Success message 
         return HttpResponseRedirect(reverse("cart"))
+    #error message
     return HttpResponseRedirect(reverse("cart"))
